c8e4dae12a3d3727f474a648fa53bc87d636cf14
dhextras <mohammedsafki27@gmail.com>
2024-09-11 16:36:33 +0530

    Print output to screen &  handle cursor movment, tab, enter, backspace

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # implementations to do
 
 - [x] Stream input in rawmode & out put it
-- [ ] Print out put properly and move around with cursor
+- [x] Print out put properly and handle cursor movment, tab, enter, backspace
 - [ ] open a file and read it's content in the editor
 - [ ] adding content to a file & then saving it
-- [ ]
 
 ## compile
 
--- editor.c
+++ editor.c
@@ -17,16 +17,26 @@ void failed(const char *func) {
 	exit(1);
 }
 
-void disableRaw() {
-	// TSCAFLUSH - waits until all pending output written to terminal
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_default) == -1) failed("tcsetattr");
+void clearTerminal()
+{
+	// clear the console & set cursor to the start
+	printf("\033[2J\033[1;1H");
+	fflush(stdout);
 }
 
-void enableRaw() {
-	if (tcgetattr(STDIN_FILENO, &termios_default) == -1) failed("tcgetattr");
-	atexit(disableRaw);
+void disableTty()
+{
+	// TSCAFLUSH - waits until all pending output written to terminal
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_default) == -1) failed("tcsetattr");
+	clearTerminal();
+}
 
-	struct termios termios_raw = termios_default;
+void enableTty()
+{
+	if (tcgetattr(STDIN_FILENO, &termios_default) == -1) failed("tcgetattr");
+	atexit(disableTty);
+
+	struct termios termios_tty = termios_default;
 
 	/* c_iflag - input flag => Disable CTRL - S,Q,C 
 	 * c_lflag - local flag => Disable echoing & canonical mode & CTRL - Z
@@ -35,32 +45,43 @@ void enableRaw() {
 	 * Other ones i have no idea why,
 	 * but some one on the internet said thats how you enable raw mode so here im..
 	 */
-	termios_raw.c_cflag |= (CS8);
-	termios_raw.c_oflag &= ~(OPOST);
-	termios_raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
-	termios_raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	termios_tty.c_cflag |= (CS8);
+	termios_tty.c_oflag &= ~(OPOST);
+	termios_tty.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+	termios_tty.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 
-	termios_raw.c_cc[VMIN] = 0; // Max byte to read set to 1
-	termios_raw.c_cc[VTIME] = 1; // Max read wait time
+	// termios_tty.c_cc[VMIN] = 0; // Max byte to read set to 1
+	// termios_tty.c_cc[VTIME] = 1; // Max read wait time
 
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_raw) == -1) failed("tcsetattr");
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_tty) == -1) failed("tcsetattr");
+	clearTerminal();
 }
 
 
 /*** init ***/
-int main() {
-	enableRaw();
+int main()
+{
+	enableTty();
 
 	while(1) {
 		char ch = '\0';
 		if (read(STDIN_FILENO, &ch, 1) == -1 && errno != EAGAIN) failed("read");
 
 		if (iscntrl(ch)) {
-			printf("%d\r\n", ch); // `\n` new line will flush the output immediately
+			if (ch == 127) {
+				printf("\b \b");
+			} else if (ch == '\x1b') {
+				printf("%c", ch);
+			} else if (ch == 13) {
+				printf("\r\n");
+			} else if (ch == 9) {
+				printf("\t");
+			}
 		} else {
-			printf("%d ('%c')\r\n", ch, ch);
+			printf("%c", ch);
 		}
 
+		fflush(stdout); // `\n` new line will flush the output immediately
 		if (ch == 'q') break;
 	};
 
